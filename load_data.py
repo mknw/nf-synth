- Added quantisation to `reduce.py` compression algo - Fixed normalisation problem with `load_celeba` loader - Command line arg config file can be given

load_data.py
@@ -119,14 +119,16 @@ def load_ffhq(batch=256, image_size=128, test=False, shuffle=False,
 			transforms.Resize(size=image_size),
 			transforms.RandomHorizontalFlip(),
 			transforms.ToTensor(),
-			transforms.Normalize(norm[0], norm[1]),
+			transforms.Normalize(norm[0], norm[1])
 		])
+		shuffle=True
 	else:
 		# test split
 		transform = transforms.Compose([
 		transforms.Resize(size=image_size),
 		transforms.ToTensor(),
-		transforms.Normalize((0.5, 0.5, 0.5), (1, 1, 1)),
+	# 	transforms.Normalize((0.5, 0.5, 0.5), (1, 1, 1)),
+		transforms.Normalize(norm[0], norm[1])
 	])
 	dataset = datasets.ImageFolder(root = path, transform=transform)
 	if not test:
@@ -138,11 +140,15 @@ def load_ffhq(batch=256, image_size=128, test=False, shuffle=False,
 	return loader
 
 
-def load_celeba(batch=32, image_size=64, test=False, shuffle=False):
+def load_celeba(batch=32, image_size=64, test=False, shuffle=False, norm=None,
+	            seed=2147483647):
 	# raise NotImplementedError("See sample_celeba")
 	''' Single loop iterator, mainly used for stats retrieval.'''
 	warnings.warn('This function should only be used for similarity'
 	              'analysis. For training a network, see `sample_celeba`.')
+	torch.manual_seed(seed)
+	if norm is None:
+		norm = ((0.5, 0.5, 0.5), (1, 1, 1)) # channel mean, std
 	if not test:
 		split = 'train'
 		transform = transforms.Compose([
@@ -150,6 +156,7 @@ def load_celeba(batch=32, image_size=64, test=False, shuffle=False):
 			transforms.Resize(size=image_size),
 			transforms.RandomHorizontalFlip(),
 			transforms.ToTensor(),
+			transforms.Normalize(norm[0], norm[1])
 		])
 		shuffle=True
 	else:
@@ -158,6 +165,7 @@ def load_celeba(batch=32, image_size=64, test=False, shuffle=False):
 		transforms.CenterCrop(160),
 		transforms.Resize(size=image_size),
 		transforms.ToTensor(),
+		transforms.Normalize(norm[0], norm[1])
 	])
 	print(f'shuffle set to {shuffle} for split: {split}')
 	target_type = ['attr', 'bbox', 'landmarks']
@@ -647,6 +655,8 @@ def load_network(model_dir, device, conf, checkpoint=True):
 		except RuntimeError as re:
 			print(re)
 			raise ArchError('There is a problem importing the model, check parameters.')
+		print(f"Resuming from epoch: {checkpoint['epoch']}"
+				f"with loss: {checkpoint['loss']}")
 
 	return net, loss_fn
 
@@ -714,7 +724,8 @@ class Attributes:
 				attribute_names = list(selection.columns)
 			complement_categs_names = ['comp_' + c for c in attribute_names]
 			# concatenate selected attributes df with its complementary.
-			selection= pd.concat([selection, (self.df.iloc[:, np.array(idxs)] == 0)], axis=1)
+			selection= pd.concat([selection, (self.df.iloc[:, idxs] == 0)], axis=1)
+			# selection= pd.concat([selection, (self.df.iloc[:, np.array(idxs)] == 0)], axis=1)
 			selection.columns = attribute_names + complement_categs_names
 		if overall_idx:
 			index_all = (selection== 1).any(axis=1)

model.py
@@ -344,6 +344,8 @@ class Block(nn.Module):
 
 
 class Glow(nn.Module):
+	''' Adapted from https://github.com/rosinality/glow-pytorch/blob/master/model.py '''
+
 	def __init__(self, in_channel, n_flow, n_block, affine=True, conv_lu=True):
 		super().__init__()
 
@@ -356,7 +358,7 @@ class Glow(nn.Module):
 
 	def forward(self, input, reverse=False, resample=False, reconstruct=False, partition=False):
 		if not reverse:
-			if resample:
+			if resample or reconstruct:
 				raise Exception('`reconstruct=True` and `resample=True` only accepted in combination \
 						with argument `reverse=True`. For reconstructing latent space from \
 						image space, set `partition=True` and `reverse=False`.')

reduce.py
@@ -16,6 +16,7 @@ from render import save_dataset_reduction, plot_compression_flow
 from similarity import track_z
 from load_data import load_celeba, load_ffhq
 from glob import glob
+from argparse import ArgumentParser
 
 
 def main(C, epoch=4340000, save=True):
@@ -32,36 +33,36 @@ def analyse_synthesizer(C, model_meta_stuff = None):
 	
 	model_root_fp, model_fp, vmarker_fp = model_meta_stuff
 	# if only plotting is needed and all vecs are stored, do not load_net 
-	if not C.no_model:
+	if not C.compress.no_model:
 		net, _ = load_network( model_fp, device, C.net)
 	epoch = model_root_fp.split('_')[-1]
 	# Load statistics:
 
-	s_str = = '-'.join(C.data) # stats-string
-	data_fn = f"{s_str}_{C.training.dataset}.npz"
+	s_str = '-'.join(map(lambda x: x.lower(), C.compress.data)) # stats-string
+	data_fn = f"{s_str}_{C.compress.dataset}.npz"
 	archive_filename = f'{model_root_fp}/{data_fn}'
 	# C.data_fn = data_fn
-	import ipdb; ipdb.set_trace()
-	potential_archive_fn = glob(f'{model_root_fp}/*{data_fn}')
-	if len(potential_archive)==1:
-		archive_filename = potential_archive_fn[0]
-		# XXX DEBUG
+	if C.compress.use_data_archive:
+		potential_archive_fn = glob(f'{model_root_fp}/*{data_fn}')
+		if len(potential_archive_fn)==1:
+			archive_filename = potential_archive_fn[0]
+			print(f"matched archive: {archive_filename}")
 
-	if os.path.isfile(archive_filename) and C.use_data_archive:
-		print(f'Loading saved arrays: `{data_fn}`')
-		print(f'and {data_fn}`', end='')
-		dataset = np.load(archive_filename)
+		if os.path.isfile(archive_filename):
+			print(f'Loading saved arrays: `{s_str}`', end='')
+			print(f' and {data_fn}`')
+			dataset = np.load(archive_filename)
 	else:
 		# Z's computation is expensive and should only
 		# be computed once. Therefore, we do it now
 		# and save it to disk.
-		if C.training.dataset == 'celeba':
+		if C.compress.dataset == 'celeba':
 			loader = load_celeba(128, C.training.img_size, test=True)
-		elif C.training.dataset == 'ffhq':
+		elif C.compress.dataset == 'ffhq':
 			loader = load_ffhq(C.training.batch_size,
 			                  C.training.img_size, test=True, shuffle=False)
-		dataset = track_z(net, device, C.training.img_size, loader, stats=C.data)
-		if C.archive_data:
+		dataset = track_z(net, device, C.training.img_size, loader, stats=C.compress.data)
+		if C.compress.archive_data:
 			np.savez(archive_filename, **dataset)
 
 	''' filepaths '''
@@ -69,61 +70,76 @@ def analyse_synthesizer(C, model_meta_stuff = None):
 
 	data_z = dataset['Z'].reshape(dataset['Z'].shape[0], -1)
 	data_x = dataset['X'].reshape(dataset['X'].shape[0], -1)
-	attributes = Attributes().fetch()
+	if C.compress.dataset == 'celeba':
+		attributes = Attributes().fetch()
+	elif C.compress.dataset == 'ffhq':
+		attributes = Attributes(dataset='ffhq')
 
 	## make cache directory for all analyses (PCA red. + reupsampling).
-	root_d = f'{C.training.root_dir}/{C.training.dataset}'
+	root_d = f'{C.training.root_dir}/compress_{C.compress.dataset}'
 	os.makedirs(root_d, exist_ok=True)
-	C.cache_bn = make_basename(root_d, cache=True)
+	# C.cache_bn = make_basename(root_d, cache=True)
 
-	# if not C.no_model:
-	# 	C.pca.n_pcs = C.pca.n_pcs_sc
-	# 	reducer = Synthesizer(C, steps = C.steps[0],
+	# if not C.compress.no_model:
+	# 	C.compress.pca.n_pcs = C.compress.pca.n_pcs_sc
+	# 	reducer = Synthesizer(C, steps = C.compress.steps[0],
 	#                       net=net, device=device)
 	# C.basename = make_basename(root_d, subdir='pca', pca_pcs=C.pca.n_pcs_sc)
 	# pca_reduction_on_attributes(C, reducer, data_z, attributes)
 
 	# re-upsampling/reconstruction visualization.
-	reup_umap_npc = [i for i in range(*eval(C.umap.n_comps))]
-	reup_pca_npc = [int((i**3.5)) for i in range(*eval(C.pca.n_pcs_reupsam))]
-	for stps in C.steps: 
+	reup_umap_npc = [i for i in range(*eval(C.compress.umap_n_comps))]
+	reup_pca_npc = [int((i**3.5)) for i in range(*eval(C.compress.pca_n_pcs))]
+	reup_pca_npc += [data_z.shape[0]-10]
+	steps_l = C.compress.steps
+	for stps in steps_l: 
 		reup_umap_l = (['empty'] if 'umap' not in stps else reup_umap_npc)
 		for umap_pcs in reup_umap_l:
-			# if not C.no_model:
+			# if not C.compress.no_model:
 			for pca_pcs in reup_pca_npc:
-				for ds in C.dataset_ratios:
-					if pca_pcs >= int(ds * data_z.shape[0]):
-						continue
+				for ds in C.compress.dataset_ratios:
 					for kept_out in [True, False]:
-						C.umap.n_comps = umap_pcs
-						C.dataset_ratio = ds
+						if (kept_out and pca_pcs >= int(ds * data_z.shape[0])-16) \
+						   or (not kept_out and pca_pcs >= int(ds * data_z.shape[0])):
+							continue
+						# in function use
+						C.steps = stps # !
+						C.ds_ratio = ds
 						C.kept_out = kept_out
-						C.pca.n_pcs = pca_pcs
+						# for Synth use:
+						C.compress.umap_n_comps = umap_pcs
+						C.compress.pca_n_pcs = pca_pcs
+						# make folders
 						C.basename = make_basename(root_d, subdir=ds, pca_pcs=pca_pcs,
 						                           umap_pcs=umap_pcs)
 						C.cache_bn = make_basename(root_d, subdir=ds, pca_pcs=pca_pcs,
-						                           umap_pcs=umap_pcs, cache=True)
+						                           umap_pcs=umap_pcs, quantise_bits=C.compress.quantise, cache=True)
 						print(f'Analysing synth with \
-						        PCA nps:{C.pca.n_pcs}, UMAP dims: {C.umap.n_comps}, \
-						        ko:{C.kept_out}, ratio:{C.dataset_ratio}')
-						if not C.no_model:
-							reducer = Synthesizer(C, steps=stps, net=net,
+						        PCA nps:{pca_pcs}, UMAP dims: {umap_pcs}, \
+						        ko:{C.kept_out}, ratio:{ds}')
+						if not C.compress.no_model:
+							reducer = Synthesizer(C.compress, steps=stps, net=net,
 						                           device=device)
 							compute_reduction_reupsampling(C, attributes, data_z,
-							                        data_x, reducer)
+							                        data_x, reducer, quantise_bits=C.compress.quantise)
 						else:
 							compute_reduction_reupsampling(C, attributes, data_z,
-							                               data_x)
+							                               data_x, quantise_bits=C.compress.quantise)
 
 
-def make_basename(root, mark_time=False, subdir=None, pca_pcs=None, umap_pcs=None, cache=False):
+def make_basename(root, mark_time=False, subdir=None, pca_pcs=None, umap_pcs=None, cache=False,
+		quantise_bits=False):
 	time = ''
 	basename = root 
 	# two subdirectories
 	if subdir:
 		# prefix (used for ratio in reupsam, otherwise analysis type ('pca'))
 		if isinstance(subdir, (int, float)):
-			basename += f'/rat{subdir}'
+			# basename += f'/rat{subdir}' # XXX !!! XXX
+			if quantise_bits:
+				basename += f'/q{quantise_bits}-rat{subdir}' # XXX !!! XXX
+			else:
+				basename += f'/noq-rat{subdir}' # XXX !!! XXX
 		else:
 			basename += f'/{subdir}'
 	if cache:
@@ -156,65 +172,87 @@ def pca_reduction_on_attributes(C, reducer, data, attributes):
 		fn = f'{C.basename}_att{i}.png'
 		save_dataset_reduction(data_red, var_exp_ratio, attributes, k=10, att_ind=i, filename=fn)
 
-def compute_reduction_reupsampling(C, attributes, data=None, data_x=None, reducer=None):
+def compute_reduction_reupsampling(C, attributes, data=None, data_x=None, reducer=None,
+		quantise_bits=8, seed=1823904943):
 	''' Save x's and z's, visualized pre (original) 
 	and post (reconstructed) dimensionality reduction.'''
-	# blond vs brown air, smiling vs. wearing hat.
-	import ipdb; ipdb.set_trace()
-	att_ind = [5, 11, 31, 35]
+	rng = np.random.default_rng(seed=seed)
+	if attributes.ds == 'celeba':
+		# Cat's: blond, brown air, smiling, wearing hat.
+		att_ind = [5, 11, 31, 35] + 1 # XXX !!! XXX
+		att_str = '-'.join(map(str, att_ind)) # [str(a) for a in att_ind])
+	elif attributes.ds == 'ffhq':
+		att_ind = 0
+		att_str = 'test-split'
 	ko = '_ko' if C.kept_out else ''
-	att_str = '-'.join([str(a) for a in att_ind])
-	ds_trunk_idx = int(data.shape[0] * C.dataset_ratio)
-	ds_rat = f'_{ds_trunk_idx}' if C.dataset_ratio != 1 else ''
+	ds_trunk_idx = int(data.shape[0] * C.ds_ratio)
+	ds_rat = f'_{ds_trunk_idx}' if C.ds_ratio != 1 else ''
 	filename = f'{C.basename}_{att_str}{ko}{ds_rat}.png'
 	cache_fn = f'{C.cache_bn}_{att_str}{ko}{ds_rat}.npz'
 
 	# middle two keys change depending on whether UMAP is used.
 	step_vec_keys = ['X', 'Z', 'PC/eigenZ', 'PC/UMAP', 'rec_Z', 'rec_X']
-	import ipdb; ipdb.set_trace()
-	### XXX debug from here (data is mis-shaped)
 
-	if C.training.dataset == 'celeba':
+	if C.compress.dataset == 'celeba':
 		kept_out_df, kept_out_idcs = attributes.pick_last_n_per_attribute(att_ind, n=4)
 		att_names = list(kept_out_df.columns)
-	else:
-		kept_out_idcs = range(16)
-		att_names = [f'row {n}' for n in range(1, 4)]
+	elif C.compress.dataset == 'ffhq':
+		# kept_out_idcs = range(16)
+		kept_out_idcs = rng.choice(5000, 16, False)
+		att_names = [f'row {n}' for n in range(1, 5)]
 
-	if C.use_step_archive and os.path.isfile(cache_fn):
+	if C.compress.use_step_archive and os.path.isfile(cache_fn):
 		step_vector = list()
 		with np.load(cache_fn) as data:
 			for k in step_vec_keys:
 				step_vector.append(data[k])
-	elif not C.no_model:
+	elif not C.compress.no_model:
 		# att_ind = list(range(0, 40, 10))
 		# split dataset
 		z_s = data[kept_out_idcs].copy()
 		x_s = data_x[kept_out_idcs].copy()
-		
+		# TODO: add individual image transform for
+		# stand-along compression
+		# if mono: 
+		# 	for d in data:
+		# 		red_d = reducer.fit_transform(d)
+		# else:
 		if C.kept_out:
 			data = np.delete(data, kept_out_idcs, axis=0)
-		rng = np.random.default_rng()
 		rng.shuffle(data, axis=0)
 		if ds_trunk_idx < data.shape[0]:
 			data = data[:ds_trunk_idx]
-
+		
 		reducer.fit(data)
-
-		del data_x; del data
 		# TODO: replace show_steps arguments with argument selection
 		red_data = reducer.transform(z_s, show_steps='all')
 
 		# the last element of red(uced)_data is the lower level representation.
+		if quantise_bits:
+			assert isinstance(quantise_bits, int)
+			quantise_lvls = 2**quantise_bits - 1
+			q_data = red_data[-1].copy()
+			q_data_min = q_data.min(axis=0, keepdims=True)
+			q_data -= q_data_min
+			q_data[q_data == 0.0] = 0.0000000000001
+			bits_ratio = quantise_lvls/q_data.max(axis=0, keepdims=True)
+			q_data *= bits_ratio
+			q_data = np.rint(q_data)
+			# inverse
+			q_data = q_data / bits_ratio + q_data_min
+			print(f"simulated quantisation with bits: {quantise_lvls+1}")
+			red_data[-1] = q_data
+
 		rec_data = reducer.inverse_transform(red_data[-1], show_steps='all',
-		                                  resample=C.training.resample)
+		                                  resample=C.compress.resample_z)
+		del data_x; del data
 		if 'umap' in C.steps:
 			step_vector = [x_s, z_s] + red_data + rec_data[1:]
 		else:
 			step_vector = [x_s, z_s] + [reducer.models['pca'].components_] \
 			               + red_data + rec_data
-		if C.archive_step:
-			np.savez(cache_fn, **dict(zip(step_vec_keys, step_vec_keys)))
+		if C.compress.archive_step:
+			np.savez(cache_fn, **dict(zip(step_vec_keys, step_vector)))
 	else:
 		raise RuntimeError
 
@@ -292,13 +330,16 @@ def plot_reduced_dataset(pca, z_s, att, k, att_ind, filename):
 
 
 if __name__ == '__main__':
-	C = ConfWrap(fn='config/rere_ffhq128_c.yml')
-	# here only for compatibility:
-	C.data = ['Z' , 'X']
 
-	C.archive_data = True
-	C.use_data_archive = True
-	C.archive_step = True
-	C.use_step_archive = True
+	conf_name = 'config/ffhq128_c.yml'
+	parser = ArgumentParser(description='RealNVP training on various datasets.')
+	parser.add_argument('--config', '-c', default=conf_name)
+	parser.parse_args()
+
+	C = ConfWrap(fn=parser.config)
+	# C.archive_data = True
+	# C.archive_step = True
+	# C.use_data_archive = False # True
+	# C.use_step_archive = False
 	main(C)
 

render.py
@@ -1,7 +1,6 @@
 #!/var/scratch/mao540/miniconda3/envs/maip-venv/bin/python
 
 from itertools import product
-from load_data import Attributes
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -143,7 +142,7 @@ def build_quadrant(step, data, axs, att_names=None, haunted=0): # , std=None):
 		##  	import ipdb; ipdb.set_trace()
 		# we normalize **parsimoniously** to preserve as much structure:
 		#1. Subtract the minimum, only for Z's with values < 0
-		try:
+		try: # should be solved (step_vector from archive was null)
 			d_min = data.min(dim, keepdims=True)
 		except TypeError:
 			import ipdb; ipdb.set_trace()
@@ -160,8 +159,10 @@ def build_quadrant(step, data, axs, att_names=None, haunted=0): # , std=None):
 		# data = (data - d_min) / (d_max) # remove extra-white aberrations
 		n_dp = data.shape[0] # n datapoints
 		img_size = (data.size // (n_dp * 3)) **.5
-		print(f'{img_size} images reshaped')
+		# print(f'{img_size} images reshaped')
 		img_size = int(img_size)
+		if img_size == 1:
+			import ipdb; ipdb.set_trace()
 		data = np.moveaxis(data.reshape(n_dp, 3, img_size, img_size), 1, -1)
 		col_count = 0
 		for row in range(4):

synthesyzer.py
@@ -21,7 +21,7 @@ from random import randrange
 class  Synthesizer(object):
 	def __init__(self, C, steps = ['net', 'pca', 'umap'],
 							net=None, device=None,
-							store_config=True, svd_solver='full'):
+							store_config=True, svd_solver='auto'):
 
 		np.random.seed(24)
 		if isinstance(steps, str):
@@ -48,16 +48,16 @@ class  Synthesizer(object):
 
 		for step in steps:
 			if step == 'pca':
-				self.models[step] = PCA(n_components=C.pca.n_pcs, whiten = C.pca.whiten,
+				self.models[step] = PCA(n_components=C.pca_n_pcs, whiten = C.pca_whiten,
 						                  random_state=42, svd_solver=svd_solver)
 			if step == 'umap':
-				self.models[step] = UMAP(n_neighbors = C.umap.nn, min_dist = C.umap.min_dist,
-			                    n_components = C.umap.n_comps, random_state=42)
+				self.models[step] = UMAP(n_neighbors = C.umap_nn, min_dist = C.umap_min_dist,
+			                    n_components = C.umap_n_comps, random_state=42)
 		if store_config:
 			self.C = C
-			self.channels = C.training.in_channels
-			self.img_size = C.training.img_size
-			self.batch = C.training.batch_size
+			self.channels = C.in_channels
+			self.img_size = C.img_size
+			self.batch = C.batch_size
 
 	def fit(self, data, skip_l=['net']):
 		''' Main fit method .

train.py
@@ -154,29 +154,33 @@ def train(config, net, device, optimizer, start_epoch, z_sample=None):
 		p_imgs += x.size(0)
 
 		if i % 1000 == 0:
+			del x
+			# torch.cuda.empty_cache()
 			# save model (note: can be factorised)
 			if i % 10000 == 0:
 				# TEST
 				model_dir = f'{config.root_dir}/epoch_{str(i).zfill(figs)}'
-				os.makedirs(model_dir) # , exist_ok=True)
+				os.makedirs(model_dir, exist_ok=True)
 				tenK_ep_ = glob(f"{config.root_dir}/epoch_*")
 				tenK_ep_.sort()
 			else:
-				tenK_ep_ = None
+				tenK_ep_ = []
 				model_dir = config.root_dir
 				
-			if loss_meter.avg < best_loss: 
+			if loss_meter.avg < best_loss or tenK_ep_:
 				torch.save({'net': net.state_dict(), 'loss': loss_meter.avg,
 				            'epoch': i},  f'{model_dir}/model.pth.tar')
 				torch.save(optimizer.state_dict(), f'{model_dir}/optim.pt')
-				with open(f'{config.root_dir}/best_model', 'w') as bm:
-					bm.write(model_dir)
-				best_loss = loss_meter.avg
-				print(f"Model with loss: {loss_meter.avg} saved to {model_dir}/model.pth.tar")
-
-			if tenK_ep_ and len(tenK_ep_) > 1:
-				for prev_epoch in tenK_ep_[:-1]:
-					rmtree(prev_epoch) # but keep backup model(s)
+				if loss_meter.avg < best_loss:
+					with open(f'{config.root_dir}/best_model', 'w') as bm:
+						bm.write(model_dir)
+					best_loss = loss_meter.avg
+					print(f"Ep.{i}: Model with loss: {loss_meter.avg} saved to {model_dir}/model.pth.tar")
+					# only remove previous checkpoints if loss improves.
+					if len(tenK_ep_) > 5:
+						for prev_epoch in tenK_ep_[:-4]:
+							rmtree(prev_epoch) # but keep backup model(s)
+			# Sampling.
 			if config.z_dist == 'normal':
 				z_sample = find_or_make_z(config.z_path, reuse=True)
 			else: raise NotImplementedError
@@ -186,7 +190,7 @@ def train(config, net, device, optimizer, start_epoch, z_sample=None):
 				                             png_filename, normalize=True,
 				                             nrow = int(config.n_samples ** 0.5))
 			del z_sample
-			torch.cuda.empty_cache()
+			# torch.cuda.empty_cache()
 			with open(f'{config.root_dir}/log', 'a') as l:
 				report = f'{loss_meter.avg:.5f},{bpd_meter.avg},{log_p.item():.5f},{p_imgs}\n'
 				l.write(report)
@@ -363,10 +367,12 @@ if __name__ == '__main__':
 	# C = ConfWrap(fn='config/ffhq256lu_c.yml')
 	# C = ConfWrap(fn='config/glow_celeba_aff.yml')
 	# C = ConfWrap(fn='config/ffhq64_gamma_c.yml')
+	conf_name = 'config/celeba128_c.yml'
+	parser = ArgumentParser(description='RealNVP training on various datasets.')
+	parser.add_argument('--config', '-c', default=conf_name)
+	parser.parse_args()
 
-	'''currently focusing on:'''
-	# C = ConfWrap(fn='config/ffhq128_c.yml')
-	C = ConfWrap(fn='config/celeba128_c.yml')
+	C = ConfWrap(fn=parser.config)
 
 	C.training.sample_dir = C.training.root_dir + '/samples'
 	main(C)