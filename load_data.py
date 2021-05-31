- fixed command line arguments - added forced stats computation for CelebA in reduce.py (in   config/ffhq128_c.yml) - Now train is logging loss and epoch to "best_model" txt file.

load_data.py
@@ -7,7 +7,6 @@ import torch
 import torchvision.transforms.functional as TF
 import torch.backends.cudnn as cudnn
 
-import warnings
 from skimage import io
 from PIL import Image, ImageDraw
 import pandas as pd
@@ -100,10 +99,10 @@ def sample_from_directory(path, batch, image_size, test=False, shuffle=False,
 			loader = iter(loader)
 			yield next(loader)
 
-def load_ffhq(batch=256, image_size=128, test=False, shuffle=False,
+def load_ffhq(batch=256, image_size=128, test=False, shuffle=None,
 	 path='data/FFHQ/thumbnails128x128', norm=None, log_path=None, seed=2147483647):
 	''' Single loop iterator, mainly used for stats retrieval.'''
-	warnings.warn('This function should only be used for similarity'
+	warn('This function should only be used for similarity'
 	              'analysis. For training a network, see `sample_from_directory`.')
 	# this takes care of all seeds througout torch processes.
 	# seed = 2147483647 is used for FFHQ split set replicability.
@@ -121,7 +120,9 @@ def load_ffhq(batch=256, image_size=128, test=False, shuffle=False,
 			transforms.ToTensor(),
 			transforms.Normalize(norm[0], norm[1])
 		])
-		shuffle=True
+		if shuffle is None:
+			shuffle = True
+			warn(f"shuffle set to: `{shuffle}' for train set")
 	else:
 		# test split
 		transform = transforms.Compose([
@@ -140,11 +141,11 @@ def load_ffhq(batch=256, image_size=128, test=False, shuffle=False,
 	return loader
 
 
-def load_celeba(batch=32, image_size=64, test=False, shuffle=False, norm=None,
+def load_celeba(batch=32, image_size=64, test=False, shuffle=None, norm=None,
 	            seed=2147483647):
 	# raise NotImplementedError("See sample_celeba")
 	''' Single loop iterator, mainly used for stats retrieval.'''
-	warnings.warn('This function should only be used for similarity'
+	warn('This function should only be used for similarity'
 	              'analysis. For training a network, see `sample_celeba`.')
 	torch.manual_seed(seed)
 	if norm is None:
@@ -158,7 +159,9 @@ def load_celeba(batch=32, image_size=64, test=False, shuffle=False, norm=None,
 			transforms.ToTensor(),
 			transforms.Normalize(norm[0], norm[1])
 		])
-		shuffle=True
+		if shuffle is None:
+			shuffle=True
+			warn(f"shuffle set to {shuffle} for train set.")
 	else:
 		split = 'test'
 		transform = transforms.Compose([

reduce.py
@@ -43,6 +43,7 @@ def analyse_synthesizer(C, model_meta_stuff = None):
 	archive_filename = f'{model_root_fp}/{data_fn}'
 	# C.data_fn = data_fn
 	if C.compress.use_data_archive:
+		import ipdb; ipdb.set_trace() # should not happen
 		potential_archive_fn = glob(f'{model_root_fp}/*{data_fn}')
 		if len(potential_archive_fn)==1:
 			archive_filename = potential_archive_fn[0]
@@ -57,7 +58,7 @@ def analyse_synthesizer(C, model_meta_stuff = None):
 		# be computed once. Therefore, we do it now
 		# and save it to disk.
 		if C.compress.dataset == 'celeba':
-			loader = load_celeba(128, C.training.img_size, test=True)
+			loader = load_celeba(128, C.training.img_size, test=True, shuffle=False)
 		elif C.compress.dataset == 'ffhq':
 			loader = load_ffhq(C.training.batch_size,
 			                  C.training.img_size, test=True, shuffle=False)
@@ -90,7 +91,7 @@ def analyse_synthesizer(C, model_meta_stuff = None):
 	# re-upsampling/reconstruction visualization.
 	reup_umap_npc = [i for i in range(*eval(C.compress.umap_n_comps))]
 	reup_pca_npc = [int((i**3.5)) for i in range(*eval(C.compress.pca_n_pcs))]
-	reup_pca_npc += [data_z.shape[0]-10]
+	reup_pca_npc += [data_z.shape[0]-16]
 	steps_l = C.compress.steps
 	for stps in steps_l: 
 		reup_umap_l = (['empty'] if 'umap' not in stps else reup_umap_npc)
@@ -178,7 +179,7 @@ def compute_reduction_reupsampling(C, attributes, data=None, data_x=None, reduce
 	and post (reconstructed) dimensionality reduction.'''
 	rng = np.random.default_rng(seed=seed)
 	if attributes.ds == 'celeba':
-		# Cat's: blond, brown air, smiling, wearing hat.
+		# Categ's: blond, brown air, smiling, wearing hat.
 		att_ind = [5, 11, 31, 35] + 1 # XXX !!! XXX
 		att_str = '-'.join(map(str, att_ind)) # [str(a) for a in att_ind])
 	elif attributes.ds == 'ffhq':
@@ -332,11 +333,11 @@ def plot_reduced_dataset(pca, z_s, att, k, att_ind, filename):
 if __name__ == '__main__':
 
 	conf_name = 'config/ffhq128_c.yml'
-	parser = ArgumentParser(description='RealNVP training on various datasets.')
-	parser.add_argument('--config', '-c', default=conf_name)
-	parser.parse_args()
+	parser = ArgumentParser(description='Compression visualisations of NF-synth.')
+	parser.add_argument('--config', '-c', default=conf_name, dest='config')
+	ap = parser.parse_args()
 
-	C = ConfWrap(fn=parser.config)
+	C = ConfWrap(fn=ap.config)
 	# C.archive_data = True
 	# C.archive_step = True
 	# C.use_data_archive = False # True

train.py
@@ -23,6 +23,7 @@ from config.config import ConfWrap
 from shutil import rmtree
 from torch.distributions.gamma import Gamma
 from glob import glob
+from argparse import ArgumentParser
 
 def main(C):
 
@@ -43,7 +44,7 @@ def main(C):
 		# C.model_dir = find_last_model_relpath(C.training.root_dir) # /model_{str(i + 1).zfill(6)}.pt'
 		if os.path.isfile(f'{C.training.root_dir}/best_model'): 
 			with open(f'{C.training.root_dir}/best_model', 'r') as bm:
-				C.model_dir = bm.read()
+				C.model_dir = bm.readline().strip()
 		print(f'Resuming from checkpoint at {C.model_dir}')
 		checkpoint = torch.load(C.model_dir+'/model.pth.tar')
 		net.load_state_dict(checkpoint['net'])
@@ -171,15 +172,17 @@ def train(config, net, device, optimizer, start_epoch, z_sample=None):
 				torch.save({'net': net.state_dict(), 'loss': loss_meter.avg,
 				            'epoch': i},  f'{model_dir}/model.pth.tar')
 				torch.save(optimizer.state_dict(), f'{model_dir}/optim.pt')
+				print(f"Ep.{i}: Model with loss: {loss_meter.avg} saved to {model_dir}/model.pth.tar")
 				if loss_meter.avg < best_loss:
-					with open(f'{config.root_dir}/best_model', 'w') as bm:
-						bm.write(model_dir)
 					best_loss = loss_meter.avg
-					print(f"Ep.{i}: Model with loss: {loss_meter.avg} saved to {model_dir}/model.pth.tar")
+					with open(f'{config.root_dir}/best_model', 'w') as bm:
+						bm.writelines([f'{model_dir}\n',
+						               f'{i}\n',
+						               f'{best_loss}\n'])
 					# only remove previous checkpoints if loss improves.
 					if len(tenK_ep_) > 5:
 						for prev_epoch in tenK_ep_[:-4]:
-							rmtree(prev_epoch) # but keep backup model(s)
+							rmtree(prev_epoch) #, but keep N backup model(s)
 			# Sampling.
 			if config.z_dist == 'normal':
 				z_sample = find_or_make_z(config.z_path, reuse=True)
@@ -367,12 +370,13 @@ if __name__ == '__main__':
 	# C = ConfWrap(fn='config/ffhq256lu_c.yml')
 	# C = ConfWrap(fn='config/glow_celeba_aff.yml')
 	# C = ConfWrap(fn='config/ffhq64_gamma_c.yml')
+	# import ipdb; ipdb.set_trace()
 	conf_name = 'config/celeba128_c.yml'
-	parser = ArgumentParser(description='RealNVP training on various datasets.')
-	parser.add_argument('--config', '-c', default=conf_name)
-	parser.parse_args()
+	parser = ArgumentParser(description='Glow training on different datasets.')
+	parser.add_argument('--config', '-c', default=conf_name, dest='config')
+	ap = parser.parse_args()
 
-	C = ConfWrap(fn=parser.config)
+	C = ConfWrap(fn=ap.config)
 
 	C.training.sample_dir = C.training.root_dir + '/samples'
 	main(C)